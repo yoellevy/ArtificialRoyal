d0614ed54304b37afac4fc947bce4af675fb3eca
Omer <OZadok@gmail.com>
2018-07-11 15:28:09 +0300

    a

--- Assets/ControllersScripts/AIController.cs
+++ Assets/ControllersScripts/AIController.cs
@@ -7,7 +7,7 @@ public class AIController : PlayerControllerScriptable
 {
     public override void CalculateNextAction()
     {
-        double[] sensorOutput = new double[8];
+        double[] sensorOutput = new double[8]; //todo
         double[] controlInputs = PlayerAgent.FNN.ProcessInputs(sensorOutput);
         move = new Vector2((float)controlInputs[0], (float)controlInputs[1]);
         shot = new Vector2((float)controlInputs[2], (float)controlInputs[3]);
