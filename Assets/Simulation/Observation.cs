2f14008ccced33186dd8dbb035b204dc71ece7bf
yoellevy <yoel.levy@mail.huji.ac.il>
2018-07-11 14:58:43 +0300

    adding observation
    adding observation

--- Assets/Simulation/Observation.cs
+++ Assets/Simulation/Observation.cs
@@ -7,16 +7,17 @@ public enum AngleQuant { None = 0, Right = 1, UpRight = 2, Up = 3, UpLeft = 4, L
 
 public class Observation : MonoBehaviour
 {
-    public float maxObservationDistance = 7;
+    public double maxObservationDistance = 7;
     public static Observation Instant = null;
     GameObject[] players = null;
     GameObject[] bullets = null;
-    
+
+    int outputSectionSize = 8;
     int outputSize;
 
     class DistanceAndAngle
     {
-        public float distance;
+        public double distance;
         /// <summary>
         ///         
         /// </summary>
@@ -28,7 +29,8 @@ public class Observation : MonoBehaviour
         }
     }
 
-    Dictionary<int, float[]> PlayerToPlayerObservation;
+    Dictionary<int, double[]> PlayerToPlayerObservation;
+    Dictionary<int, double[]> PlayerToBulletObservation;
 
     DistanceAndAngle[,] PlayerToPlayerDistance;
     DistanceAndAngle[,] PlayerToBulletDistance;
@@ -53,11 +55,11 @@ public class Observation : MonoBehaviour
     {
         outputSize = 0;
         if (observeBullets)
-            outputSize += 8;
+            outputSize += outputSectionSize;
         if (observePlayers)
-            outputSize += 8;
+            outputSize += outputSectionSize;
         if (observeWalls)
-            outputSize += 8;
+            outputSize += outputSectionSize;
     }
 
     bool flag = true;
@@ -68,49 +70,57 @@ public class Observation : MonoBehaviour
         if (flag)
         {
             players = GameObject.FindGameObjectsWithTag("PlayerTag");
-            PlayerToPlayerObservation = new Dictionary<int, float[]>();
-            for (int i = 0; i < players.Length; i++)               
-                PlayerToPlayerObservation[players[i].GetComponent<PlayerScript>().id] = new float[8];
+            PlayerToPlayerObservation = new Dictionary<int, double[]>();
+            PlayerToBulletObservation = new Dictionary<int, double[]>();
+            for (int i = 0; i < players.Length; i++)
+            {
+                int currId = players[i].GetComponent<PlayerScript>().id;
+                PlayerToPlayerObservation[currId] = new double[outputSectionSize];
+                PlayerToBulletObservation[currId] = new double[outputSectionSize];
+            }
 
             PlayerToPlayerDistance = new DistanceAndAngle[players.Length, players.Length];
             flag = false;
         }
         bullets = GameObject.FindGameObjectsWithTag("Bullet");
         calculateDistanceFromPlayers();
-        //calculateDistanceFromBullet();
+        calculateDistanceFromBullet();
     }
+
     private void OnGUI()
     {
-        //for (int i = 0; i < 8; i++)
-        //{
-        //    GUI.Label(new Rect(10, 10+i*20, 100, 20), PlayerToPlayerObservation[0][i].ToString(), new GUIStyle { fontSize = 20 });
-        //}
+        for (int i = 0; i < 8; i++)
+        {
+            GUI.Label(new Rect(10, 10 + i * 20, 100, 20), PlayerToPlayerObservation[0][i].ToString(), new GUIStyle { fontSize = 20 });
+        }
+        for (int i = 0; i < 8; i++)
+        {
+            GUI.Label(new Rect(300, 10 + i * 20, 100, 20), PlayerToBulletObservation[0][i].ToString(), new GUIStyle { fontSize = 20 });
+        }
         ////GUI.Label(new Rect(10, 10, 100, 20), PlayerToPlayerObservation[0].ToString(), new GUIStyle { fontSize = 20 });
         //GUI.Label(new Rect(150, 50, 100, 20), MathHelper.GetAngle(Vector3.right, players[1].transform.position - players[0].transform.position).ToString(), new GUIStyle { fontSize = 20 });
     }
 
     private void calculateDistanceFromPlayers()
     {
-        foreach (var item in PlayerToPlayerObservation)     
-            for (int i = 0; i < 8; i++)            
+        foreach (var item in PlayerToPlayerObservation)
+            for (int i = 0; i < outputSectionSize; i++)
                 item.Value[i] = maxObservationDistance;
-          
+
         for (int i = 0; i < players.Length; i++)
         {
             int p1id = players[i].GetComponent<PlayerScript>().id;
-            for (int j = i+1; j < players.Length; j++)
+            for (int j = i + 1; j < players.Length; j++)
             {
                 int p2id = players[j].GetComponent<PlayerScript>().id;
 
-                float currDistance = Vector2.Distance(players[i].transform.position, players[j].transform.position);
+                double currDistance = Vector2.Distance(players[i].transform.position, players[j].transform.position);
                 if (currDistance < maxObservationDistance)
-                {                    
+                {
                     AngleQuant currAngle = calculateAngle(players[i].transform.position, players[j].transform.position);
                     AngleQuant opAngle = oppositeAngle(currAngle);
-                    PlayerToPlayerObservation[i][(int)currAngle - 1] = currDistance;
-                    PlayerToPlayerObservation[j][(int)opAngle - 1] = currDistance;
-                    if (i == 0)
-                        print(currAngle);
+                    PlayerToPlayerObservation[p1id][(int)currAngle - 1] = currDistance;
+                    PlayerToPlayerObservation[p2id][(int)opAngle - 1] = currDistance;
                 }
             }
         }
@@ -118,20 +128,21 @@ public class Observation : MonoBehaviour
 
     private void calculateDistanceFromBullet()
     {
-        for (int i = 0; i < players.Length; i++)
-        {
-            for (int j = 0; j < bullets.Length; j++)
-            { PlayerToBulletDistance[i, j] = new DistanceAndAngle { distance = maxObservationDistance, angle = 0 }; }
-        }
+        foreach (var item in PlayerToBulletObservation)
+            for (int i = 0; i < outputSectionSize; i++)
+                item.Value[i] = maxObservationDistance;
 
         for (int i = 0; i < players.Length; i++)
         {
-            for (int j = 0; j < bullets.Length; j++)
+            int p1id = players[i].GetComponent<PlayerScript>().id;
+            foreach (var item in bullets)
             {
-                float currDistance = Vector2.Distance(players[i].transform.position, bullets[j].transform.position);
-
-                AngleQuant currAngle = calculateAngle(players[i].transform.position, bullets[j].transform.position);
-                PlayerToBulletDistance[i, j] = new DistanceAndAngle { distance = currDistance, angle = currAngle };
+                double currDistance = Vector2.Distance(item.transform.position, players[i].transform.position);
+                if (currDistance < maxObservationDistance)
+                {
+                    AngleQuant currAngle = calculateAngle(players[i].transform.position, item.transform.position);
+                    PlayerToBulletObservation[p1id][(int)currAngle - 1] = currDistance;
+                }
             }
         }
     }
@@ -173,13 +184,13 @@ public class Observation : MonoBehaviour
 
     private AngleQuant calculateAngle(Vector3 position1, Vector3 position2)
     {
-        float angle = MathHelper.GetAngle(Vector3.right, position2 - position1);
+        double angle = MathHelper.GetAngle(Vector3.right, position2 - position1);
 
         if (angle <= 22.5f || angle >= 337.5f)
             return AngleQuant.Right;
-        float prevAngle = 22.5f;
+        double prevAngle = 22.5f;
         int i = 2;
-        for (float currAngle = prevAngle; currAngle < 360; currAngle += 45, i++)
+        for (double currAngle = prevAngle; currAngle < 360; currAngle += 45, i++)
             if (angle >= currAngle && angle <= currAngle + 45)
             {
                 return (AngleQuant)Enum.ToObject(typeof(AngleQuant), i);
@@ -196,10 +207,24 @@ public class Observation : MonoBehaviour
     /// </summary>
     /// <param name="id">id of player to return the observation of</param>
     /// <returns>obseration </returns>
-    public float[] getObservationOfPlayerID(int id)
+    public double[] getObservationOfPlayerID(int id)
     {
         //for now only player observation 
         //TODO: extend to all observation
-        return PlayerToPlayerObservation[id];
+        double[] outputObservation = new double[outputSize];
+        int idx = 0;
+        if (observePlayers)
+        {
+            PlayerToPlayerObservation[id].CopyTo(outputObservation, idx);
+            idx += outputSectionSize;
+        }
+
+        if (observeBullets)
+        {
+            PlayerToBulletObservation[id].CopyTo(outputObservation, idx);
+            idx += outputSectionSize;
+        }
+
+        return outputObservation;
     }
 }
